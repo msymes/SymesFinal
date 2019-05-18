879875e9aa4b0470f784a29001e7e8403bae389d
msyme <msyme@DESKTOP-IUC8NCA>
2019-05-18 18:57:14 -0400

    almost

---
+++ StudentLoanCalc/src/main/java/app/Loan.java
@@ -0,0 +1,121 @@
+package app;
+
+import java.util.LinkedList;
+import org.apache.poi.ss.formula.functions.*;
+public class Loan {
+	double dLoanAmount;
+	double dInterestRate;
+	double iTerm;
+	double dFutureValue = 0;
+	boolean bInterestCalc = true;
+	double dExtraPayment;
+	public int numberPayments = (int)iTerm*12;
+	public double totalInterest;
+	
+	public LinkedList<Payment> loanPayments = new LinkedList<Payment>();
+	
+	
+	public Loan(double dLoanAmount, double dInterestRate, double iTerm, double ExtraPayment) {
+		super();
+		this.dLoanAmount = dLoanAmount;
+		this.dInterestRate = dInterestRate;
+		this.iTerm = iTerm;
+		this.dExtraPayment = ExtraPayment;
+		Amortization();
+	}
+	
+		
+	
+	public void Amortization(){
+		double newbal;
+		double month_interest = (dInterestRate / 12);
+		int number_payments = (int)iTerm * 12;
+		double interest_payment;
+		double principal_payment;
+		double balance = dLoanAmount;
+		int i =0;
+
+		double payment = dLoanAmount * month_interest /
+               ( 1 - 1 / Math.pow(1 + month_interest, number_payments) );
+		
+		for(i=0; i<number_payments;i++) {
+			if(balance<(payment+dExtraPayment)) {
+				break;
+			}
+			interest_payment = balance * month_interest;
+			principal_payment = payment - interest_payment + dExtraPayment; 
+			newbal = balance - principal_payment; 
+			balance = newbal; 
+			Payment p = new Payment(payment, interest_payment,principal_payment,i,balance);
+			loanPayments.add(p);
+			totalInterest += interest_payment;
+		}
+		// last month
+		principal_payment = balance;
+		interest_payment = balance * month_interest;
+		payment = principal_payment + interest_payment;
+		newbal = 0.0;
+		balance = 0.0;
+		Payment p = new Payment(payment, interest_payment,principal_payment,i+1,balance);
+		loanPayments.add(p);
+		numberPayments = i;
+		totalInterest+= interest_payment;
+
+}
+	public class Payment {
+		private double Payment;
+		private double Interest;
+		private double Principal;
+		private int PaymentNumber;
+		private double Balance;
+		
+		public double getPayment() {
+			return Payment;
+		}
+
+		public void setPayment(double payment) {
+			Payment = payment;
+		}
+
+		public double getInterest() {
+			return Interest;
+		}
+
+		public void setInterest(double interest) {
+			Interest = interest;
+		}
+
+		public double getPrincipal() {
+			return Principal;
+		}
+
+		public void setPrincipal(double principal) {
+			Principal = principal;
+		}
+
+		public int getPaymentNumber() {
+			return PaymentNumber;
+		}
+
+		public void setPaymentNumber(int paymentNumber) {
+			PaymentNumber = paymentNumber;
+		}
+
+		public double getBalance() {
+			return Balance;
+		}
+
+		public void setBalance(double balance) {
+			Balance = balance;
+		}
+
+		public Payment(double payment, double interest, double principal, int number, double Balance) {
+			this.Payment = payment;
+			this.Interest = interest;
+			this.Principal = principal;
+			this.PaymentNumber = number;
+			this.Balance = Balance;
+		}
+	}
+}
+	
---
+++ StudentLoanCalc/src/main/java/app/controller/LoanCalcViewController.java
@@ -0,0 +1,136 @@
+package app.controller;
+
+import app.Loan;
+import app.Loan.Payment;
+import app.StudentCalc;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import java.net.URL;
+import java.time.LocalDate;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.scene.control.DatePicker;
+
+import javafx.application.Application;
+
+import javafx.beans.property.SimpleStringProperty;
+
+import javafx.collections.FXCollections;
+
+import javafx.collections.ObservableList;
+
+import javafx.event.ActionEvent;
+
+import javafx.event.EventHandler;
+
+import javafx.geometry.Insets;
+
+import javafx.scene.Group;
+
+import javafx.scene.Scene;
+
+import javafx.scene.control.Button;
+
+import javafx.scene.control.Label;
+
+import javafx.scene.control.TableColumn;
+
+import javafx.scene.control.TableView;
+
+import javafx.scene.control.TextField;
+
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import javafx.scene.layout.HBox;
+
+import javafx.scene.layout.VBox;
+
+import javafx.scene.text.Font;
+
+import javafx.stage.Stage;
+
+public class LoanCalcViewController implements Initializable   {
+
+	private StudentCalc SC = null;
+	
+	@FXML
+	private TextField LoanAmount;
+	
+	@FXML
+	private TextField InterestRate;
+	
+	@FXML
+	private TextField Term;
+	
+	@FXML
+	private TextField AdditionalPayment;
+
+	@FXML
+	private Label lblTotalPayemnts;
+	
+	@FXML
+	private Label lblTotalInterest;
+	
+	@FXML
+	private DatePicker PaymentStartDate;
+	
+	
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+	}
+
+	public void setMainApp(StudentCalc sc) {
+		this.SC = sc;
+	}
+	
+	     
+		    
+		    
+	/**
+	 * btnCalcLoan - Fire this event when the button clicks
+	 * 
+	 * @version 1.0
+	 * @param event
+	 */
+	@FXML
+	private void btnCalcLoan(ActionEvent event) {
+		double dInterestRate = Double.parseDouble(InterestRate.getText());
+		double dTerm = Double.parseDouble(Term.getText());
+		double dAdditionalPayment = Double.parseDouble(AdditionalPayment.getText());
+		double dLoanAmount = Double.parseDouble(LoanAmount.getText());
+		Loan L = new Loan(dLoanAmount, dInterestRate, dTerm, dAdditionalPayment);
+		for (Payment p :  L.loanPayments) {
+			System.out.println(p.getPaymentNumber());
+			System.out.println(p.getPayment());
+			System.out.println(p.getInterest());
+			System.out.println(p.getPrincipal());
+			System.out.println(p.getBalance());
+		}
+			
+	
+
+
+		lblTotalPayemnts.setText(Integer.toString(L.numberPayments));
+		lblTotalInterest.setText(Double.toString(L.totalInterest));
+		
+		LocalDate localDate = PaymentStartDate.getValue();
+	 
+		System.out.println(localDate);
+
+	    
+
+	 
+}
+
+	}
+	
+
+		 
+
+	    
